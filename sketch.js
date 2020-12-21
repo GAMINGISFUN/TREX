59ea305856742069cb1b3b2852480d9673cb2ec8
GAMINGISFUN <75155978+GAMINGISFUN@users.noreply.github.com>
2020-12-21 20:58:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,40 +1,29 @@
-var car, wall;
-var speed, weight;
-
+var wall,thickness;
+var speed,weight,bullet;
+var lbullet,lwall
 function setup() {
   createCanvas(1600,400);
-  car=createSprite(200, 200, 50, 50);
-  wall=createSprite(1200, 200, 60, 100);
-
-  car.velocityX = 5 ;
-
-  speed=random(25,31);
-  weight=random(400,1500);
-
+  bullet = createSprite(1200, 100, 50, 20);
+  bullet.shapeColor = "white" ;
+  speed = Math.round(random(223,321));
+  weight = Math.round(random(30,42));
+  thickness = Math.round(random(22,83));
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = color(80,80,80) ;
+  bullet.velocityX = speed ;
 }
 
 function draw() {
-  background("black");  
-
-if (wall.x-car.x<(car.width+wall.width)/2){
-  car.velocityX=0;
-  var deformation=0.5*weight*speed*speed/22509
-  if (deformation>180)
-  {
-    car.shapeColor=color("yellow");
-    wall.shapeColor=color("white");
+  background("black");
+  if(hasCollided(bullet,wall)) {
+    bullet.velocityX = 0;
+    var damage = 0.5 * weight * speed * speed/(thickness *thickness *thickness);
+    if(damage > 10) {
+      wall.shapeColor = color(255,0,0);
+    }
+    if(damage< 10) {
+      wall.shapeColor = color(0,255,0);
+    }
   }
-  if (deformation <180 && deformation>100)
-  {
-    car.shapeColor=color("green");
-    wall.shapeColor=color("white");
-  }
-  if (deformation<100)
-  {
-    car.shapeColor=color("red");
-    wall.shapeColor=color("white");
-  }
-}
-
   drawSprites();
 }
