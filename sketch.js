1ced783c4c60256072900463ed24367f29e2818e
GAMINGISFUN <75155978+GAMINGISFUN@users.noreply.github.com>
2020-12-21 21:29:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,29 +1,65 @@
-var wall,thickness;
-var speed,weight,bullet;
-var lbullet,lwall
-function setup() {
-  createCanvas(1600,400);
-  bullet = createSprite(1200, 100, 50, 20);
-  bullet.shapeColor = "white" ;
-  speed = Math.round(random(223,321));
-  weight = Math.round(random(30,42));
-  thickness = Math.round(random(22,83));
-  wall = createSprite(1200,200,thickness,height/2);
-  wall.shapeColor = color(80,80,80) ;
-  bullet.velocityX = speed ;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
 }
 
-function draw() {
-  background("black");
-  if(hasCollided(bullet,wall)) {
-    bullet.velocityX = 0;
-    var damage = 0.5 * weight * speed * speed/(thickness *thickness *thickness);
-    if(damage > 10) {
-      wall.shapeColor = color(255,0,0);
-    }
-    if(damage< 10) {
-      wall.shapeColor = color(0,255,0);
-    }
-  }
-  drawSprites();
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.3, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
 }
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on
+    Matter.Body.setStatic(packageBody,false)
+  }
+}
+
+
+
